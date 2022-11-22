fd2041098b1392a32502927709cfb8b0466f5bf2
Xianting Zhang <zhan1301@msu.edu>
2022-11-22 03:36:51 -0500

    Done

--- soundboard.js
+++ soundboard.js
@@ -1,14 +1,11 @@
-function playDesert() {
-    var desert = new Audio('Desert.wav');
-    desert.play();
-}
-  
-function playForest() {
-var forest = new Audio('Forest.wav');
-forest.play();
-}
-  
-function playWild() {
-var wild = new Audio('Wild.wav');
-wild.play();
+var button_1 = document.getElementById("desert");
+var button_2 = document.getElementById("forest");
+var button_3 = document.getElementById("wild");
+
+var desert = new Audio('Desert.wav');
+var forest = new Audio('Forest.wav')
+var wild = new Audio('Wild.wav')
+
+function playSound(sound) {
+    sound.play();
 }
