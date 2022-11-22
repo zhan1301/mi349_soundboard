310bc644739960802638ede361185bb837dd1eb8
Xianting Zhang <zhan1301@msu.edu>
2022-11-22 02:39:40 -0500

    Some changes

---
+++ soundboard.js
@@ -0,0 +1,14 @@
+function playDesert() {
+    var desert = new Audio('Desert.wav');
+    desert.play();
+}
+  
+function playForest() {
+var forest = new Audio('Forest.wav');
+forest.play();
+}
+  
+function playWild() {
+var wild = new Audio('Wild.wav');
+wild.play();
+}
